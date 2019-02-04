e403aae1e6d8aea6140451c0a429d6a860064962
IoganVII IoganV <IoganVII@yandex.ru>
2019-02-04 17:53:41 +0300

    Первый пошёл!

---
+++ Scottish duel/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+using Scottish_duel.Models;
+
+namespace Scottish_duel.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private SqlConnection sqlConnection = null;
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            return View();
+        }
+
+        [HttpPost]
+        public RegisterModel Reg (RegisterModel model)
+        {
+
+            model.Login = model.Login;
+
+            return new RegisterModel()
+            {
+                Login = "123",
+                Password = "456"
+            };
+        }
+
+
+    }
+}
